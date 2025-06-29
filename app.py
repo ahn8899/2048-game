873898e3962bada7c522bf4ca87b53bc638ab4ea
ahn8899 <168278229+ahn8899@users.noreply.github.com>
2025-06-29 20:42:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -353,5 +353,5 @@ def submit_score():
         return jsonify({'success': False, 'error': str(e)})
 
 # ========= 稳定的入口 ==========
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
